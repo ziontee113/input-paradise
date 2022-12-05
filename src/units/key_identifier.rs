5a306787ce8611f06ad91689bb20948dd5bec9b7
ziontee113 <ziontee113@gmail.com>
2022-12-05 21:08:59 +0700

    refactor: change key_identifier constructor
    In production, use ::new() constructor to create KeyIdentifier
    from device_alias and fetched event .code()
    
    For testing purposes, use ki!() macro, which itself uses the
    .from_str() method to create a KeyIdentifier.

--- src/units/key_identifier.rs
+++ src/units/key_identifier.rs
@@ -3,7 +3,7 @@ use super::keycode::key_code_from_str;
 #[macro_export]
 macro_rules! ki {
     ($a:ident $b:ident) => {
-        $crate::units::key_identifier::KeyIdentifier::new(stringify!($a), stringify!($b))
+        $crate::units::key_identifier::KeyIdentifier::from_str(stringify!($a), stringify!($b))
     };
 }
 
@@ -11,15 +11,20 @@ macro_rules! ki {
 pub struct KeyIdentifier {
     device_alias: String,
     code: u16,
-    key_name: String,
 }
 
 impl KeyIdentifier {
-    pub fn new(device_alias: &str, key_name: &str) -> Self {
+    pub fn new(device_alias: &str, code: u16) -> Self {
+        Self {
+            device_alias: device_alias.to_string(),
+            code,
+        }
+    }
+
+    pub fn from_str(device_alias: &str, key_name: &str) -> Self {
         Self {
             device_alias: device_alias.to_uppercase(),
             code: key_code_from_str(key_name).unwrap(),
-            key_name: key_name.to_uppercase(),
         }
     }
 
@@ -32,20 +37,20 @@ impl KeyIdentifier {
     }
 }
 
-impl std::fmt::Display for KeyIdentifier {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} {}", self.device_alias, self.key_name)
-    }
-}
-
 #[cfg(test)]
 mod test {
-    #[test]
-    fn can_use_macro_to_create_key_identifier() {
-        let key = ki!(L1 D);
-        assert_eq!(key.to_string(), "L1 D");
+    use crate::units::key_identifier::KeyIdentifier;
 
-        let key = ki!(r1 j);
-        assert_eq!(key.to_string(), "R1 J");
+    fn can_construct_key_identifier_from_device_alias_and_code() {
+        let key = KeyIdentifier::new("L1", 32);
+        assert_eq!(key.device_alias(), "L1");
+        assert_eq!(key.code(), 32);
+    }
+
+    #[test]
+    fn can_use_macro_to_create_key_identifier_for_testing_purposes() {
+        let key = ki!(L1 D);
+        assert_eq!(key.device_alias(), "L1");
+        assert_eq!(key.code(), 32);
     }
 }
