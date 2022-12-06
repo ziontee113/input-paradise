f88cbe2f6a1230f3d4ecd7d1b8abe8f3c4ee7429
ziontee113 <ziontee113@gmail.com>
2022-12-07 03:36:24 +0700

    Spawn threads in timer_experience

--- src/timer_experience.rs
+++ src/timer_experience.rs
@@ -1,12 +1,31 @@
-use std::{thread, time::Duration};
+use std::{sync::mpsc, thread, time::Duration};
 
 use tokio::task;
 
 pub fn timer_experience() {
-    task::spawn_blocking(|| {
-        thread::sleep(Duration::from_millis(50));
-        println!("hello world");
+    let (tx, rx) = mpsc::channel();
+    let tx2 = tx.clone();
+
+    task::spawn_blocking(move || {
+        for i in 0..10 {
+            thread::sleep(Duration::from_millis(50));
+            tx.send(format!("first loop - {i}")).unwrap();
+        }
     });
 
-    println!("hello venus");
+    task::spawn_blocking(move || {
+        for i in 0..10 {
+            thread::sleep(Duration::from_millis(50));
+            tx2.send(format!("second loop - {i}")).unwrap();
+        }
+    });
+
+    task::spawn_blocking(move || {
+        for msg in rx {
+            println!("{msg}");
+        }
+        println!("finish last");
+    });
+
+    println!("finish first");
 }
